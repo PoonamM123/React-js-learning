4fea877ae6de8466b4fc7e4e6bc8dc21fc8b21fa
poonam <poonammehandiya111@gmail.com>
2025-07-10 23:10:21 +0530

    prop children

---
+++ 01fundamental/04propschildren/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import {  Card, Small } from './components/card'
+
+function App() {
+
+  return (
+    <>
+    <Card>
+      <h1>poonam mehandiya</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga hic perferendis consequuntur suscipit.</p>
+      <Small></Small>
+    </Card>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01fundamental/04propschildren/src/components/card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const Card= ({children}) => {
+    return(
+        
+        <><h1>po</h1>
+        {children}</>
+    )
+}
+
+const Small=()=>{
+    return (
+        <h1>Poonam</h1>
+    )
+}
+export { Card, Small };
+
+
